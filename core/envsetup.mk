1e173fbcce80dbde5bdfedf1e86c08f9a85a17bb
Dylan Simon <dylan@dylex.net>
2012-12-31 17:52:03 -0500

    fix architecture: x86_64
    Change-Id: Ib48649e7139caa76e9a210d09d682d8436b7f8cd

--- core/envsetup.mk
+++ core/envsetup.mk
@@ -87,7 +87,11 @@ endif
 
 # HOST_ARCH
 ifneq (,$(findstring 86,$(UNAME)))
+ifneq (,$(findstring _64,$(UNAME)))
+	HOST_ARCH := x86_64
+else
 	HOST_ARCH := x86
+endif
 endif
 
 ifneq (,$(findstring Power,$(UNAME)))
