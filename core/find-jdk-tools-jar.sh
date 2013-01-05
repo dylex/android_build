1bce8feaa1ec76e348d185ba040dda7608b50835
Dylan Simon <dylan@dylex.net>
2013-01-05 12:31:23 -0500

    fix find-jdk-tools-jar for relative symlinks
    Change-Id: I306acc5fa9225745277058e1a00a6afe539dca41

--- core/find-jdk-tools-jar.sh
+++ core/find-jdk-tools-jar.sh
@@ -6,9 +6,6 @@ else
 	echo "Please-install-JDK-6.0,-which-you-can-download-from-java.sun.com"
 	exit 1
     fi
-    while [ -L $JAVAC ] ; do
-        LSLINE=$(ls -l $JAVAC)
-        JAVAC=$(echo -n $LSLINE | sed -e "s/.* -> //")
-    done
+    JAVAC=$(realpath $JAVAC)
     echo $JAVAC | sed -e "s:\(.*\)/bin/javac.*:\\1/lib/tools.jar:"
 fi
